7db0a2df8296463de51b2a44a6ebfa07a3da2e47
kriminal666 <kriminalaverno@gmail.com>
2015-05-16 19:53:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+#Laravel package skeleton
+A basic skeleton for laravel packages
+
+## Getting started
+
+ - Clone this repo: `git clone git@github.com:digital-baker/laravel-package-skeleton.git`
+ - Edit composer.json (author, packagename, dependencies and namespace)
+ - Rename `Providers\PackageServiceProvider` to {YourPackageServiceProvider} and update Namespace
+ - Update namespace of tests for your namespace (in phpunit.xml and tests/bootstrap.php!)
+ - Done!
---
+++ src/Providers/EvaluationModuleServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+/*
+*
+* (c) Marco Bunge <marco_bunge@web.de>
+*
+* For the full copyright and license information, please view the LICENSE.txt
+* file that was distributed with this source code.
+* 
+*/
+namespace Criminal\Evaluation\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class EvaluationModuleServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+    
+        /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        
+    }
+}
